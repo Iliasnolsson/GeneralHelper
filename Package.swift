43478f62c7a10d5df858065272d8be1e111cae0c
Iliasnolsson <Iliasnolsson@icloud.com>
2022-06-24 12:17:23 +0200

    Test

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .library(
             name: "Helper",
             targets: [
-                "File"
+                "Helper"
             ]),
     ],
     dependencies: [
@@ -25,6 +25,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "Helper",
+            dependencies: []),
         .target(
             name: "File",
             dependencies: []),
