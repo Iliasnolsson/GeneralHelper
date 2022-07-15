ccaa2dd0de69d3853d7b1b0b544b9c992b510972
Iliasnolsson <Iliasnolsson@icloud.com>
2022-07-15 21:52:54 +0200

    identifier gettable for header & footer

---
+++ Sources/GeneralHelper/Extensions/UIKit/UITableViewHeaderFooterView.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ilias Nikolaidis Olsson on 2022-07-15.
+//
+
+import UIKit
+
+public extension UITableViewHeaderFooterView {
+    
+    static var identifier: String {.init(describing: Self.self)}
+    
+}
